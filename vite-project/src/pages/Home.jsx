4e540107a9265ad57205516b9391bcc0ca34a743
Sefaciftci <sefaciftci.cs@gmail.com>
2024-05-05 18:26:08 +0300

    home page responsive design

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import LoadingPage from "./LoadingPage";
 import imgSrc from '../img/home.png';
+import { FaUser } from "react-icons/fa";
+
 
 const Home = () => {
 
@@ -21,32 +23,32 @@ const Home = () => {
     <div >
       <div className="min-h-screen bg-purple-100">
 
-        <div className="mx-32 py-6">
+        <div className="px-8 md:mx-32 py-3 md:py-6">
 
           <nav className="flex justify-between items-center color-purple-500">
-            <h3 className="text-5xl color-purple font-cursive tracking-wide	font-semibold">
+            <h3 className="text-3xl md:text-5xl color-purple font-cursive tracking-wide	font-semibold">
               DOPPY
             </h3>
-            <ul className="flex gap-20 text-xl text-purple-950 font-medium">
-              <li className="hover:underline hover:border-purple-500 transition-all duration-500 cursor-pointer">
+            <ul className="flex gap-8 md:gap-20 text-lg md:text-xl text-purple-950 font-medium">
+              <li className="hover:border-purple-500 hover:text-purple-500  transition-all duration-500 cursor-pointer">
                 Home
               </li>
-              <li className="hover:underline border-purple-500 transition-all duration-300 cursor-pointer">
+              <li className="border-purple-500 hover:text-purple-500 transition-all duration-300 cursor-pointer">
                 About
               </li>
-              <li className="hover:underline border-purple-500 transition-all duration-300 cursor-pointer">
+              <li className="border-purple-500 hover:text-purple-500 transition-all duration-300 cursor-pointer">
                 Quiz
               </li>
             </ul>
 
-            <div className="flex gap-2">
+            <div className="flex gap-2 md:gap-3">
               <button
-                className="py-1 px-1.5 text-sm font-semibold border-2 text-purple-900 cursor-pointer border-purple-900 rounded-full hover:bg-purple-900 hover:text-white hover:border-white transition-all duration-500 "
+                className="text-xl md:text-xl font-bold text-purple-900 cursor-pointer   hover:text-purple-500  transition-all duration-500 "
               >
-                <span>{user.displayName}</span>
+                <span><FaUser /></span>
               </button>
               <button
-                className="py-1.5 px-8 text-xl font-semibold border-2 text-purple-900 cursor-pointer border-purple-900 rounded-2xl hover:bg-purple-900 hover:text-white hover:border-white transition-all duration-500 "
+                className=" py-1 md:py-1.5 px-5 md:px-8 text-xl font-semibold border-2 text-purple-900 cursor-pointer border-purple-900 rounded-2xl hover:bg-purple-900 hover:text-white hover:border-white transition-all duration-500 "
                 onClick={handleSignOut}
               >
                 Sign Out
@@ -55,33 +57,33 @@ const Home = () => {
           </nav>
 
 
-          <div className="pt-32 flex flex-row px-5">
+          <div className="pt-14 md:pt-24 flex flex-col-reverse md:flex-row px-3 md:px-5">
 
-            <div className="flex flex-col justify-center text-left basis-1/2">
-              <h3 className="text-4xl font-semibold mb-6 font-mono w-1/2">
+            <div className="flex flex-col justify-center text-left md:basis-1/2">
+              <h3 className="text-3xl md:text-4xl font-semibold mb-6 font-mono md:w-1/2">
                 easy ways to work with a team
               </h3>
               <p
-                className="text-lg font-light mb-10">
+                className=" text-base md:text-lg font-light mb-6 md:mb-10">
                 this is my original illustration for the home page of kerjain.
                 this platform for a team to work on their work remotely without
                 having to meet.
               </p>
 
-              <a className="text-center py-3 px-10 text-xl font-semibold border-2 rounded-2xl text-purple-900 cursor-pointer border-purple-900  hover:bg-purple-900 hover:text-white hover:border-white transition-all duration-500 w-56">
+              <a className="text-center py-2 md:py-3 md:px-10 w-56 text-xl font-semibold border-2 rounded-2xl text-purple-900 cursor-pointer border-purple-900  hover:bg-purple-900 hover:text-white hover:border-white transition-all duration-500 ">
                 get started
               </a>
             </div>
 
 
 
-            <div className="basis-1/2">
+            <div className="md:basis-1/2">
 
               <div className="flex justify-center items-center">
                 <img
                   src={imgSrc}
                   alt="social media"
-                  className="w-128 h-128 object-cover border-none"
+                  className="w-96 h-96 md:w-128 md:h-128  object-cover border-none"
                 />
               </div>
 
